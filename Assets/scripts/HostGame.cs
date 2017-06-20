fa4f7ce20dc56b2ee89c1f00be5e35fce6229f69
farguen <igesfe97b@gmail.com>
2017-06-20 20:30:46 +0200

    first commit

---
+++ Assets/scripts/HostGame.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+
+    [SerializeField]
+    private uint roomSize = 6;
+
+    private string roomName;
+
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomName(string _name)
+    {
+        Debug.Log("seteando");
+        roomName = _name;
+        Debug.Log("jum: " + roomName);
+    }
+
+    public void CreateRoom()
+    {
+        
+        if (roomName != "" && roomName != null)
+        {
+            Debug.Log("Creando");
+            Debug.Log("Creating Room: " + roomName + " with room for " + roomSize + " players.");
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+
+}
